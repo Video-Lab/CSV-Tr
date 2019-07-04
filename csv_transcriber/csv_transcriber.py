a99b4ee6c56a883b65880f484b650c0b7bd9f0e4
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-04 14:58:00 -0500

    Formatting

--- csv_transcriber/csv_transcriber.py
+++ csv_transcriber/csv_transcriber.py
@@ -4,21 +4,20 @@ import os
 
 def transcribe(i, o):
 
-	if not os.path.isdir(o):
+	if not os.path.isdir(o): # If dir doesnt exist, create.
 		os.mkdir(o)
 
-	with open(i, mode='r') as csv_file:
+	with open(i, mode='r') as csv_file: # Open CSV File
+
 		re = list(csv.reader(csv_file))
 		headers = re[0]
-		for row in re[1:]:
-			with open(f'{o}/{row[0]}.txt', mode="w+") as file:
-				for key in range(len(headers)):
-					file.write(f'{headers[key]}: {row[key]}\n')
+
+		for row in re[1:]: # Loop through rows
+
+			with open(f'{o}/{row[0]}.txt', mode="w+") as file: # Create text file in that directory with name as first row
+
+				for key in range(len(headers)): # Loop through properties of row
+
+					file.write(f'{headers[key]}: {row[key]}\n') # Write to text file with format: 'Name of Row: Value'
+
 		return
-	# Open CSV File
-	# If dir doesnt exist, create.
-	# Loop through rows
-	# Create text file in that directory with name as first row
-	# Loop through properties of row
-	# Write to text file with format: 'Name of Row: Value'
-	# Return directory
