c943bcb99cab9a20a6387178f4e0a655c8d6befd
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-04 10:40:30 -0500

    Pseudocode for transcribe function, import modules

---
+++ csv_transcriber/csv_transcriber.py
@@ -0,0 +1,11 @@
+import csv
+import sys
+
+def transcribe(i, o):
+	# Open CSV File
+	# If dir doesnt exist, create.
+	# Loop through rows
+	# Create text file in that directory with name as first row
+	# Loop through properties of row
+	# Write to text file with format: 'Name of Row: Value'
+	# Return directory
