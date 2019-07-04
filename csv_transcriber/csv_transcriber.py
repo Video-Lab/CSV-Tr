06223ac57213d421a207fbb6372bcecaefb9dc5c
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-04 18:58:32 -0500

    Flexibility of paths & arguments

--- csv_transcriber/csv_transcriber.py
+++ csv_transcriber/csv_transcriber.py
@@ -7,12 +7,17 @@ def transcribe(i, o):
 	if not os.path.isdir(o): # If dir doesnt exist, create.
 		os.mkdir(o)
 
+	if o[len(o)-1] == "/": # Take slash off end for flexibility of function
+		o = o.strip("/")
+
 	with open(i, mode='r') as csv_file: # Open CSV File
 
 		re = list(csv.reader(csv_file))
 		headers = re[0]
 
 		for row in re[1:]: # Loop through rows
+
+
 
 			with open(f'{o}/{row[0]}.txt', mode="w+") as file: # Create text file in that directory with name as first row
 
@@ -21,3 +26,7 @@ def transcribe(i, o):
 					file.write(f'{headers[key]}: {row[key]}\n') # Write to text file with format: 'Name of Row: Value'
 
 		return
+	
+	
+	
+	
