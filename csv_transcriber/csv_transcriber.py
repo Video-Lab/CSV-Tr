d4e4e23dc0a7654d5efbcd3073c6e362990ac113
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-04 11:12:39 -0500

    Defined transcribe function

--- csv_transcriber/csv_transcriber.py
+++ csv_transcriber/csv_transcriber.py
@@ -1,7 +1,23 @@
 import csv
 import sys
+import os
 
 def transcribe(i, o):
+
+	if !os.path.isdir(o):
+		os.mkdir(o)
+
+	with open(i, mode='r') as csv_file:
+		reader = csv.DictReader(csv_file)
+		count = 0
+		for row in reader:
+			if count == 0:
+				headers = row
+			else:
+				with open(f'{o}/{row[rows[0]]}.txt', mode="w+") as file:
+					for key in rows:
+						file.write(f'{key}: {row[key]}\n')
+		return
 	# Open CSV File
 	# If dir doesnt exist, create.
 	# Loop through rows
